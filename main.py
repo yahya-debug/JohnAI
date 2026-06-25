1b513548995e6d2aaebbbbf494e4b02edc3af53f
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-06-25 12:15:49 +0300

    now our agent have the ability to save the previous messages in the same chat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,10 +12,21 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
+
 def main():
+    
+    parser = argparse.ArgumentParser(description="JohnAI")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        model='gemini-2.5-flash', contents=messages
     )
+
+
     if response is None:
         raise RuntimeError("failed API request")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
