9fff2e7c65f81429d50e8a2fa628552cd2ed921e
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-06-25 18:36:31 +0300

    adding system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,7 +15,6 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    
     parser = argparse.ArgumentParser(description="JohnAI")
     parser.add_argument("user_prompt", type=str, help="User Prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -24,7 +24,7 @@ def main():
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages
+        model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
     )
 
 
